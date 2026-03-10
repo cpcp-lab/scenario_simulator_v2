d4a3b6eac2146ec62595b655e3da3a0c08dda975
dsksh <dsksh@users.noreply.github.com>
2026-03-10 01:25:20 +0000

    deploy: 7686c5216fb82051eb95a16af7ba9987b6fb10a0

---
+++ search/defines_d.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['unexpected_5fenumeration_5fvalue_5fassigned_7282',['UNEXPECTED_ENUMERATION_VALUE_ASSIGNED',['../error_8hpp.html#a795e0525e2a2edcf7b8b7d57b0e433d3',1,'error.hpp']]],
+  ['unexpected_5fenumeration_5fvalue_5fspecified_7283',['UNEXPECTED_ENUMERATION_VALUE_SPECIFIED',['../error_8hpp.html#a7452037427f36552b186821326eb1e19',1,'error.hpp']]],
+  ['unsupported_5fcatalog_5freference_5fspecified_7284',['UNSUPPORTED_CATALOG_REFERENCE_SPECIFIED',['../catalog__reference_8cpp.html#a35ceaab153845790f93d943461a99f4e',1,'catalog_reference.cpp']]],
+  ['unsupported_5fconversion_5fdetected_7285',['UNSUPPORTED_CONVERSION_DETECTED',['../error_8hpp.html#a94339408bbb4df7d4e2bedb59bf6f016',1,'error.hpp']]],
+  ['unsupported_5felement_5fspecified_7286',['UNSUPPORTED_ELEMENT_SPECIFIED',['../error_8hpp.html#a9f0d570886e18a23cea90e768e5e3916',1,'error.hpp']]],
+  ['unsupported_5fenumeration_5fvalue_5fspecified_7287',['UNSUPPORTED_ENUMERATION_VALUE_SPECIFIED',['../error_8hpp.html#a9be8ee6629f41fa56e1dd04fb9dbad47',1,'error.hpp']]],
+  ['unsupported_5fsetting_5fdetected_7288',['UNSUPPORTED_SETTING_DETECTED',['../error_8hpp.html#a4a0739b58615cbc03a525b86eea0b412',1,'error.hpp']]]
+];
---
+++ search/namespaces_b.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['utils_4069',['utils',['../namespaceutils.html',1,'']]]
+];
