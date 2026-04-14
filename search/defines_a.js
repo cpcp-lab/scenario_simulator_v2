aade66ef8058dfb3d3e7ce0ad9ac4c742734b622
dsksh <dsksh@users.noreply.github.com>
2026-04-14 02:01:28 +0000

    deploy: 7686c5216fb82051eb95a16af7ba9987b6fb10a0

--- search/defines_a.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['read_5fcategory_7261',['READ_CATEGORY',['../catalog_8cpp.html#a7caaa5a254967e989cdb2ae423689cbe',1,'catalog.cpp']]],
-  ['requires_7262',['REQUIRES',['../requires_8hpp.html#a96848b74000220f6531e6f7ba197a296',1,'requires.hpp']]]
-];
