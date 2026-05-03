bf38814175e2ae3f16f245ee970eb521d99b93e4
dsksh <dsksh@users.noreply.github.com>
2026-05-03 02:24:41 +0000

    deploy: 66ca79a57b7cdd1b7e0e2498fe3dcb05e3106063

--- search/defines_2.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['case_7386',['CASE',['../conversions_8cpp.html#a15ebaacdae475d3733193bcbb6f34960',1,'CASE():&#160;conversions.cpp'],['../object_8hpp.html#aa3afe3681aca3c0d19bd04f187e830dd',1,'CASE():&#160;object.hpp']]],
-  ['concealer_5fexport_7387',['CONCEALER_EXPORT',['../external_2concealer_2include_2concealer_2visibility_8hpp.html#ab447f25f8bc28d9e314e01ace9ef83ce',1,'visibility.hpp']]],
-  ['concealer_5fimport_7388',['CONCEALER_IMPORT',['../external_2concealer_2include_2concealer_2visibility_8hpp.html#a5c725623cf54b3813332da8dd72c806e',1,'visibility.hpp']]],
-  ['concealer_5flocal_7389',['CONCEALER_LOCAL',['../external_2concealer_2include_2concealer_2visibility_8hpp.html#a30d48c4139db19798d25e4a8af6559a7',1,'visibility.hpp']]],
-  ['concealer_5fpublic_7390',['CONCEALER_PUBLIC',['../external_2concealer_2include_2concealer_2visibility_8hpp.html#a09c2b2d5d6f1e64d2dfdb7d3ca0deead',1,'visibility.hpp']]],
-  ['concealer_5fpublic_5ftype_7391',['CONCEALER_PUBLIC_TYPE',['../external_2concealer_2include_2concealer_2visibility_8hpp.html#a159d5a50e5e93a1bc710e71034f28635',1,'visibility.hpp']]]
-];
