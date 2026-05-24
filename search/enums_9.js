51f94b65338dadeee9d9c619e27f31156f173984
dsksh <dsksh@users.noreply.github.com>
2026-05-24 02:42:21 +0000

    deploy: 66ca79a57b7cdd1b7e0e2498fe3dcb05e3106063

---
+++ search/enums_9.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['testresult_7161',['TestResult',['../namespacejunit.html#a08b542fc650dd2a1dae63f6ddfbafb0e',1,'junit']]],
+  ['trajectoryshape_7162',['TrajectoryShape',['../namespacetraffic__simulator_1_1lane__change.html#aef862ca2fc8f7f6b5ac705535b551d4a',1,'traffic_simulator::lane_change']]],
+  ['transition_7163',['Transition',['../namespacetraffic__simulator_1_1speed__change.html#a3092a9e94ebf9abbe27f987c47265b04',1,'traffic_simulator::speed_change']]],
+  ['transportprotocol_7164',['TransportProtocol',['../namespacesimulation__interface.html#a6d935a4e115a42b775e3aac5aa8795a3',1,'simulation_interface']]],
+  ['type_7165',['Type',['../structtraffic__simulator_1_1lane__change_1_1Constraint.html#af78f4085ec21fe876ba5d40c58587866',1,'traffic_simulator::lane_change::Constraint::Type()'],['../structtraffic__simulator_1_1speed__change_1_1Constraint.html#a37f8e1c8c77e767d8694a1228caf736b',1,'traffic_simulator::speed_change::Constraint::Type()'],['../structtraffic__simulator_1_1speed__change_1_1RelativeTargetSpeed.html#a5af0c0c4dc1b9130e796fb521af61237',1,'traffic_simulator::speed_change::RelativeTargetSpeed::Type()'],['../namespacetraffic__simulator_1_1job.html#a8602b55ae10946708527359bebc639e0',1,'traffic_simulator::job::Type()']]]
+];
